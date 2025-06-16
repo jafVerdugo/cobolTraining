e8d8ea80eab8717389a3fc60a4dae44884800ac4
jafVerdugo <jafverdugo@gmail.com>
2025-06-16 16:03:33 +0200

    Create Times.cbl

---
+++ Times.cbl
@@ -0,0 +1,17 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. cobolTimes.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world".
+       PERFORM MAIN-PROCEDURE 10 TIMES.
+       STOP RUN.
+       END PROGRAM cobolTimes.
