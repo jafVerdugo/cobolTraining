aaed86283c39618404e3494567917a2ad46ee7c3
jafVerdugo <jafverdugo@gmail.com>
2025-06-24 11:17:00 +0200

    test images+ cobolcheck

--- cobolCheck.cbl
+++ cobolCheck.cbl
@@ -12,6 +12,9 @@
        01 num1 PIC 9 VALUE 1.
        01 num2 PIC 9 VALUE 1.
        01 result PIC 9.
+       01 word1 PIC x(8) VALUE "hello".
+       01 word2 PIC x(8) VALUE "world".
+       01 word3 PIC x(13).
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
             ADD num1 TO num2 GIVING result.
@@ -21,5 +24,10 @@
             COMPUTE result = num1 + num2.
             DISPLAY num2.
             DISPLAY result.
+            STRING word1 DELIMITED BY SIZE
+                   word2 DELIMITED BY SIZE
+                   INTO word3
+            END-STRING.
+            DISPLAY word3.
             STOP RUN.
        END PROGRAM CobolCheck.
